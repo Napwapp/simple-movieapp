4589a6a50447d3e211bab6ad3fc70b3703dcda61
Napwapp <abduhnawaf@gmail.com>
2025-07-08 16:21:11 +0700

    deploy vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: '/simple-movieapp',
-  assetPrefix: '/simple-movieapp',
 };
 
 export default nextConfig;
